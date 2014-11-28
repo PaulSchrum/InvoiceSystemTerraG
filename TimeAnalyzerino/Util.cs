b6ba34d6832f987c6ac193a34fdc1f7f0148b290
Paul Schrum <paul.schrum@gmail.com>
2014-11-27 22:03:16 -0500

    Added static Util clas

---
+++ TimeAnalyzerino/Util.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeAnalyzerino
+{
+   public static class Util
+   {
+      public static bool AreAnyNull(params Object[] p)
+      {
+         return p.Any(param => null == param);
+      }
+
+   }
+}
